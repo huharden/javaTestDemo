721f158ef7c971b71b1268b90a6ee6b0d17f99bd
Administrator <15170073501@163.com>
2020-01-03 18:30:38 +0800

    多线程synchronized方法测试

---
+++ src/main/java/com/brock/smootbursty/controller/Test01.java
@@ -0,0 +1,73 @@
+package com.brock.smootbursty.controller;
+
+import java.util.Hashtable;
+
+/**
+ * Description:
+ *
+ * @author: hutao
+ * Date: 2019-12-27-11:44
+ */
+public class Test01 {
+
+    static long value = 0l;
+
+    String box1 = "a";
+    String box2 = "b";
+
+     long get() {
+        synchronized(box1){
+
+            return value;
+        }
+
+
+    }
+
+     void add() {
+         synchronized(box2){
+
+             try {
+                 Thread.sleep(3000);
+                 value += 1;
+             } catch (InterruptedException e) {
+                 e.printStackTrace();
+             }
+         }
+
+
+
+    }
+
+
+    public static void main(String[] args) {
+
+        Test01 test01 = new Test01();
+
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                test01.add();
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("aaaa===>>>"+test01.get());
+            }
+        }).start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                long b = test01.get();
+                b = b + 1;
+                System.out.println("bbbb===>>>"+b);
+            }
+        }).start();
+
+    }
+
+
+}
