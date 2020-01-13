5a5ecb4712e76b616186ee9ff059d152ddd33bf5
Administrator <15170073501@163.com>
2020-01-13 18:12:49 +0800

    生成xml文件代码

---
+++ src/main/java/com/brock/smootbursty/model/PublicParameter.java
@@ -0,0 +1,44 @@
+package com.brock.smootbursty.model;
+
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+
+/**
+ *
+ */
+
+@Data
+@Accessors(chain = true)
+public class PublicParameter implements Serializable {
+
+  private static final long serialVersionUID = 1L;
+
+  /**
+   * 证书秘钥
+   */
+  private String client_secret;
+
+  /**
+   * 证书id
+   **/
+  private String client_id;
+
+  /**
+   * 系统当前时间戳
+   **/
+  private String timestamp;
+
+  /**
+   * Xml被Base64加密后得到的报文
+   **/
+  private String exxml;
+
+  /**
+   * 加密签名
+   **/
+  private String sign;
+
+
+}
---
+++ src/main/java/com/brock/smootbursty/service/ContinuingEducationService.java
@@ -0,0 +1,10 @@
+package com.brock.smootbursty.service;
+
+import com.brock.smootbursty.model.XmlParameter;
+
+import java.io.File;
+
+public interface ContinuingEducationService {
+
+    String getToken(XmlParameter xmlParameter);
+}
