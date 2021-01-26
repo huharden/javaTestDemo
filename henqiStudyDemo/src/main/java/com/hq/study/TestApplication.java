8272a9a9a508c71add33abbf61ea482e48b22b9d
lijipeng <li.jipeng@iwhalecloud.com>
2021-01-26 18:24:49 +0800

    aaaa

--- henqiStudyDemo/src/main/java/com/hq/study/TestApplication.java
+++ henqiStudyDemo/src/main/java/com/hq/study/TestApplication.java
@@ -14,7 +14,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TestApplication {
     public static void main(String[] args) {
         SpringApplication.run(TestApplication.class,args);
-        System.out.println("TestApplication................");
+        System.out.println("TestApplication...................");
     }
 
 }
